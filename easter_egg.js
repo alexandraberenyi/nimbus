7e8f94503b9baf95161d1dcfac4a05be7b0a1784
alexandraberenyi <alexandra.berenyi@gmail.com>
2022-12-14 21:06:04 +0100

    easter

--- easter_egg.js
+++ easter_egg.js
@@ -2,12 +2,23 @@ var clicks = 0;
 
 function easterEgg(var_id) {
   clicks++;
+  console.log(clicks)
   var x = document.getElementById(var_id);
   if (clicks>=5) {
-    if (x.className === "easter") {
+    if (x.className === "easter_egg") {
       x.className += " surp";
+      clicks = 0;
     } else {
-      x.className = "easter";
+      x.className = "easter_egg";
     }
   }
+
 } 
+
+function easterEggRemove(var_id) {
+  clicks++;
+  if (clicks>=2) {
+    document.getElementById(var_id).classList.remove("surp");
+    clicks = 0;
+  }
+}
