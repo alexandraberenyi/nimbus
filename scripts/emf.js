0e24996f903eb599356756686c821646b3370d15
Alexandra Berényi <101739400+alexandraberenyi@users.noreply.github.com>
2023-04-20 15:22:28 +0200

    emf modify

--- scripts/emf.js
+++ scripts/emf.js
@@ -17,32 +17,23 @@ for (const div of document.querySelectorAll('p')) {
 
 console.log(matches); // 👉️ [div.box]*/
 
-function includeHTML() {
-  var z, i, elmnt, file, xhttp;
-  /* Loop through a collection of all HTML elements: */
-  z = document.getElementsByTagName("*");
-  console.log(z)
-  for (i = 0; i < z.length; i++) {
-    elmnt = z[i];
-    /*search for elements with a certain atrribute:*/
-    file = elmnt.getAttribute("w3-include-html");
-    if (file) {
-      /* Make an HTTP request using the attribute value as the file name: */
-      xhttp = new XMLHttpRequest();
-      xhttp.onreadystatechange = function() {
-        if (this.readyState == 4) {
-          if (this.status == 200) {elmnt.innerHTML = this.responseText;}
-          if (this.status == 404) {elmnt.innerHTML = "Page not found.";}
-          /* Remove the attribute, and call this function once more: */
-          elmnt.removeAttribute("w3-include-html");
-          includeHTML();
-        }
-      }
-      xhttp.open("GET", file, true);
-      xhttp.send();
-      /* Exit the function: */
-      return;
-    }
-  }
+for(var j = 23; j <=50 ; j++){
+
+  $.get('/oktatas/metfuzet/EMF0' + j + '/EMF' + j + '.htm').done(function () {
+    emfszam = j;
+
+    $( "#noID" ).load( "oktatas/metfuzet/EMF0" +j+ "/EMF" +j+ ".htm #noID" );
+    $( "#editorID" ).load( "oktatas/metfuzet/EMF0" +j+ "/EMF" +j+ ".htm #editorID" );
+    $( "#titleID" ).load( "oktatas/metfuzet/EMF0" +j+ "/EMF" +j+ ".htm #titleID" );
+    $( "#yearID" ).load("oktatas/metfuzet/EMF0" +j+ "/EMF" +j+ ".htm #yearID" );
+    document.getElementById("titleID").setAttribute("href", "index.html");
+
+
+    //console.log(emfszam)
+  }).fail(function () {
+    //console.log("failed.");
+  });
 }
-includeHTML();
+
+
+
